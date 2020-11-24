fb7ec638f4e784b0a7d84a3ae1bde3840d32cbd2
Joseph Cooper <cooper.joseph@gmail.com>
2020-11-24 11:45:57 +0000

    Refactor least[X]Residue functions

--- src/main/kotlin/com/omricat/kommons/math/Number.kt
+++ src/main/kotlin/com/omricat/kommons/math/Number.kt
@@ -1,20 +1,40 @@
 package com.omricat.kommons.math
 
-fun Long.leastNonNegativeResidue(modulus: Long): Long {
+fun Long.leastNonNegativeResidue(modulus: Long): Long =
+    leastResidue(LeastResidueType.NON_NEGATIVE, modulus)
+
+fun Long.leastPositiveResidue(modulus: Long): Long =
+    leastResidue(LeastResidueType.POSITIVE, modulus)
+
+fun Int.leastNonNegativeResidue(modulus: Int): Int =
+  leastResidue(LeastResidueType.NON_NEGATIVE, modulus)
+
+fun Int.leastPositiveResidue(modulus: Int): Int =
+  leastResidue(LeastResidueType.POSITIVE, modulus)
+
+@Suppress("NOTHING_TO_INLINE")
+private inline fun Int.leastResidue(type: LeastResidueType, modulus: Int): Int {
   require(modulus > 0) { "Modulus must be positive, was $modulus" }
   val residue = this % modulus
-  return if (residue >= 0)
+  return if (residue >= type.mustBeAtLeast)
     residue
   else
     residue + modulus
 }
 
-fun Int.leastNonNegativeResidue(modulus: Int): Int {
+@Suppress("NOTHING_TO_INLINE")
+private inline fun Long.leastResidue(
+  type: LeastResidueType,
+  modulus: Long
+): Long {
   require(modulus > 0) { "Modulus must be positive, was $modulus" }
   val residue = this % modulus
-  return if (residue >= 0)
+  return if (residue >= type.mustBeAtLeast)
     residue
   else
     residue + modulus
 }
+
+private enum class LeastResidueType(val mustBeAtLeast: Int) { NON_NEGATIVE(0), POSITIVE(1) }
+
 
